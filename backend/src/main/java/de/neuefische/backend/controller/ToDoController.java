4112d0a38e04869b00370c3b366b59b1310d6451
Lars Schlögel <lars.schloegel@gmail.com>
2021-09-24 10:52:07 +0200

    Anfang Controller

---
+++ backend/src/main/java/de/neuefische/backend/controller/ToDoController.java
@@ -0,0 +1,23 @@
+package de.neuefische.backend.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/todo")
+public class ToDoController {
+
+    private final ToDoService toDoService;
+
+    @Autowired
+    public ToDoController(ToDoService toDoService) {
+        this.toDoService = toDoService;
+    }
+
+    @GetMapping
+    public
+
+}
