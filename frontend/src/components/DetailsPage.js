21b891f9c973e5357f865ec04e1d2f7b6ba862f2
Lars Schlögel <lars.schloegel@gmail.com>
2021-10-07 11:53:14 +0200

    Ergänzung UseSingleTodo

--- frontend/src/components/DetailsPage.js
+++ frontend/src/components/DetailsPage.js
@@ -3,7 +3,7 @@ import useSingleTodo from "../hooks/useSingleTodo";
 export default function DetailsPage() {
 
     const {todo} = useSingleTodo()
-
+//
     return (
         <div>
             <h2>ID: {todo.id}</h2>
